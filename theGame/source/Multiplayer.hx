d71985596fc69b48f0aa1cdd2c1ffd07bf14b27a
Forbrig <vitorforbrig@gmail.com>
2019-05-30 09:44:19 -0300

    networking extension for multiplayer

--- theGame/source/Multiplayer.hx
+++ theGame/source/Multiplayer.hx
@@ -2,7 +2,19 @@ package;
 
 import flixel.*;
 
+import networking.Network;
+import networking.sessions.Session;
+import networking.utils.NetworkEvent;
+import networking.utils.NetworkMode;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.Sprite;
+import openfl.Lib;
+import openfl.events.MouseEvent;
+
 class Multiplayer extends FlxSprite {
+    public static inline var IP_SERVIDOR:String = "127.0.0.1";
+
     public static inline var OP_NOVO_JOGADOR:String = "n"; // [OP, ]
     public static inline var OP_MOVE:String = "m"; // [OP, _idJogador, x, y, x.velocity, y.velocity]
     public static inline var OP_TIRO:String = "t"; // [OP, _idJogador, xp, yp]
@@ -13,11 +25,30 @@ class Multiplayer extends FlxSprite {
     public function new() {
         super();
         _idJogador = -1;
+        criaCliente();
+    }
+
+    function criaCliente():Void {
+
+        var client = Network.registerSession(NetworkMode.CLIENT, {
+            ip: IP_SERVIDOR,
+            port: 8888,
+            flash_policy_file_url: 'http://' + IP_SERVIDOR + ':9999/crossdomain.xml'
+        });
+
+        client.addEventListener(NetworkEvent.MESSAGE_RECEIVED, function(event: NetworkEvent) {
+            FlxG.log.add(event.data);
+        });
+
+        client.start();
     }
 
     public function sendMsg(msg:Array<Any>) {
         // acoxambrando comuncacao
-        onMsg(msg);
+        // onMsg(msg);
+
+        var session = Network.sessions[0];
+        session.send(msg);
     }
 
     // retorna true se precisa processar a mensagem
@@ -95,17 +126,15 @@ class Multiplayer extends FlxSprite {
 
         _count += e;
 
-        if (_count >= 3) {
-            _count = 0;
-            sendMsg([OP_MOVE, 1, 100, 150, 20, 20]);
-            sendMsg([OP_MOVE, 2, 100, 150, 0, 20]);
-            sendMsg([OP_MOVE, 3, 100, 150, 0, 80]);
-            sendMsg([OP_MOVE, 4, 100, 150, 80, 80]);
-            sendMsg([OP_TIRO, 4, 100, 150]);
-            // sendMsg(null);
-        }
-
-        
+        // if (_count >= 3) {
+        //     _count = 0;
+        //     sendMsg([OP_MOVE, 1, 100, 150, 20, 20]);
+        //     sendMsg([OP_MOVE, 2, 100, 150, 0, 20]);
+        //     sendMsg([OP_MOVE, 3, 100, 150, 0, 80]);
+        //     sendMsg([OP_MOVE, 4, 100, 150, 80, 80]);
+        //     sendMsg([OP_TIRO, 4, 100, 150]);
+        //     // sendMsg(null);
+        // }
 
         super.update(e);
     }
